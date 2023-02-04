a0c4490ec14093259f972cff406e0e8103cd69f6
Jordi <jordirosado@jordirosado.com>
2023-02-04 14:29:21 +0100

    feat: creacion model competitor

---
+++ models/Competitor.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema, model} from "mongoose";
+
+const CompetitorSchema = new Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        junior:{
+            type:String,
+            required:true
+        },
+        senior:{
+            type:String,
+            required:true
+        },
+        female:{
+            type:String,
+            required:true
+        },
+        male:{
+            type:String,
+            required:true
+        },
+        weight_female:{
+            type:String,
+            required:true
+        },
+        weight_male:{
+            type:String,
+            required:true
+        }
+
+    }
+);
+
+
+const Competitor = model("competitors", CompetitorSchema);
+
+export default Competitor;
