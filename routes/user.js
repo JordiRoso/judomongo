c52fd97110fd202e895bfc952c79d61fbb125d4f
Jordi <jordirosado@jordirosado.com>
2023-02-08 11:51:26 +0100

    feat: creado los middelwares

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 const router = Router();
 
 import UserController from "../controllers/UserController.js";
+import isSuperAdmin from "../middelwares/isSuperAdmin.js";
+import verifyToken from "../middelwares/verifyToken.js";
 
-router.get("/",UserController.getAll)
+
+router.get("/users", verifyToken, isSuperAdmin, UserController.getAll);
+router.get("/",UserController.getAll);
 
 
 
